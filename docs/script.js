c027712ffd44f52eb34a5301fed159d80b1b9aeb
ricald <ricald21@gmail.com>
2021-07-04 18:40:30 +0000

    TOPへ戻るボタンの設置

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,8 @@
 ﻿$(function () {
+  var pagetopAppear = false;
+  var pagetop = $('#page_top');
+
+  // スクロールアニメーション
   $(window).scroll(function () {
     const wHeight = $(window).height();
     const scrollAmount = $(window).scrollTop();
@@ -8,6 +12,24 @@
         $(this).addClass("scrollin");
       }
     });
+
+    if (scrollAmount > 100) {
+      if (pagetopAppear == false) {
+        pagetopAppear = true;
+        pagetop.stop().animate({ 'right': '1.5rem' }, 500);
+      }
+    } else {
+      if (pagetopAppear == true) {
+        pagetopAppear = false;
+        pagetop.stop().animate({ 'right': '-5rem' }, 500);
+      }
+    }
+  });
+
+  // TOPへ戻る
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 1000);
+    return false;
   });
 });
 
